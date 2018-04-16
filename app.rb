e96cf3af286a56794795706f74239852f8e3b93f
Ryan Laughlin <rofreg@gmail.com>
2018-04-15 22:21:32 -0400

    Add talks page

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
+
+get '/talks' do
+  send_file File.join(settings.public_folder, 'talks', 'index.html')
+end
+
+get '/talks/rubyconf2017' do
+  send_file File.join(settings.public_folder, 'talks', 'rubyconf2017.pdf')
+end
+
+get '/talks/railsconf2018' do
+  send_file File.join(settings.public_folder, 'talks', 'railsconf2018.pdf')
+end
